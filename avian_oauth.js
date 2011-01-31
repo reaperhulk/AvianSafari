0c9c2a0f735040018e9b35b8e291ff0b5baea04b
Paul Kehrer <paul.l.kehrer@gmail.com>
2011-01-31 14:27:13 -0600

    relicensed as dual license. modified BSD or GPL v2. knock yourself out

--- avian_oauth.js
+++ avian_oauth.js
@@ -2,21 +2,7 @@
 AvianLib: Javascript OAuth for Twitter
 Primarily designed for Twitter, this is an OAuth lib written entirely in Javascript (for use in JS environments not bound by cross-domain restrictions.  Appcelerator, WebOS, Safari extensions, Chrome extensions, et cetera). Avian itself is specific to Twitter, but the OAuth object (found in avianlib_oauth.js) should be easily used for different OAuth applications.
 
-    Copyright 2010 Paul Kehrer
-
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+Dual Licensed Modified BSD (3-clause) and GPL v2
 */
 
 function OAuth(params) {
